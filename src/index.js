6cf9253031dbecf4f96336d1d5b6c9ca9c583bcf
David Moreno <dmoreno788@gmail.com>
2019-11-03 12:33:14 +0100

    Arreglada ruta de Reducers en index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 import { Provider}  from 'react-redux';
 import { createStore, applyMiddleware  } from 'redux';
-import RootReducers from './reducers/index';
+import RootReducers from './redux/reducers';
 import thunk from 'redux-thunk';
 
 const store = createStore(RootReducers,applyMiddleware(thunk));
